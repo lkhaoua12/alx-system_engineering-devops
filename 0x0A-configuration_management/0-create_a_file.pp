e97cfc6b9d9447ca41fbebacbf9cda9f7504de60
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-22 23:01:53 +0100

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,12 +1,3 @@
-/**
- * Create a file in /tmp with specific permissions, owner, and group.
- * @file_path: The path to the file to be created.
- * @permissions: The file permissions (e.g., '0744').
- * @owner: The owner of the file.
- * @group: The group of the file.
- * @content: The content to be written to the file.
- */
-
 file { '/tmp/school':
   ensure  => 'file',
   mode    => '0744',
