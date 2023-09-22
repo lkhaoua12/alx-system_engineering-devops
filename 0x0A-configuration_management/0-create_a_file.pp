fdb5b81444e74f373ec1abfc1b685b339fd94b14
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-22 22:52:44 +0100

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,16 +1,16 @@
 /**
  * Create a file in /tmp with specific permissions, owner, and group.
- *
  * @file_path: The path to the file to be created.
  * @permissions: The file permissions (e.g., '0744').
  * @owner: The owner of the file.
  * @group: The group of the file.
  * @content: The content to be written to the file.
  */
+
 file { '/tmp/school':
-  ensure	=> 'file',
-  mode	=> '0744',
-  owner	=> 'www-data',
-  group	=> 'www-data',
-  content	=> 'I love Puppet',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
