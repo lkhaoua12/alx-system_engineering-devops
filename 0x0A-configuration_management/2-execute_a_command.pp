4efa19bef6b3036d7aec8c12d9a866a2d00e03de
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-28 01:19:19 +0100

    run exec ressource

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,11 +1,5 @@
 # define an exec ressourse that kills the killmenow procces.
 exec {'killmenow': 
   command => 'pkill -f "killmenow"',
-  path => '/bin:/usr/bin',
-  refreshonly => true,
-}
-
-# notify the exec resource to tun whene refreshed.
-notify {'run killmenow':
-  subscribe => Exec['killmenow'],
+  provider => 'shell',
 }
