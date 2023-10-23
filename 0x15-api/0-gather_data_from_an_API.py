3bda2fd27360c528ee5ae278e23a014010ad298b
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-24 00:48:44 +0100

    fixed formatting

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,29 @@
-#!/usr/bin/python3
-""" Fetch and diplay the to-do list """
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+if len(sys.argv) != 2:
+    print("Usage: python3 script_name.py <employee_id>")
+    sys.exit(1)
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+employee_id = sys.argv[1]
+
+url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+response = requests.get(url)
+
+if response.status_code != 200:
+    print("Employee not found")
+    sys.exit(1)
+
+employee_data = response.json()
+employee_name = employee_data['name']
+
+url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+response = requests.get(url)
+todos = response.json()
+
+completed_tasks = [todo for todo in todos if todo['completed']]
+total_tasks = len(todos)
+
+print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
+for todo in completed_tasks:
+    print(f"\t{todo['title']}")
