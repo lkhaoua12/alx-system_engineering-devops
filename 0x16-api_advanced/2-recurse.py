695fb8484079713ae9c156e59672fbb36b19f5ad
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-11-08 02:28:43 +0100

    fetch all hot pages

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,25 +4,40 @@ import requests
 import sys
 
 
-def recurse(subreddit, hot_list=[], count=0, next_page=None):
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {'user-agent': 'alx api advanced'}
-    params = {"count": count, "next_page": next_page, "limit": 50}
-    response = requests.get(
-        url, params=params, headers=headers, allow_redirects=False
-                            )
+def recurse(subreddit, hot_list=[], after=None):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Alx/api"}
+
+    if after:
+        params = {"limit": 100, "after": after}
+    else:
+        params = {"limit": 100}
+
+    response = requests.get(url, headers=headers, params=params)
+
     if response.status_code != 200:
         return None
+
     data = response.json().get("data")
-    next_page = data.get("next_page")
-    count += data.get("dist")
+
+    if data is None:
+        return None
+
     children = data.get("children")
+
+    if not children:
+        return hot_list
+
     for child in children:
-        title = child.get("title")
+        title = child.get("data").get("title")
         hot_list.append(title)
-    if next_page is not None:
-        return recurse(subreddit, hot_list, count, next_page)
-    return hot_list
+
+    after = data.get("after")
+
+    if not after:
+        return hot_list
+    else:
+        return recurse(subreddit, hot_list, after)
 
 
 if __name__ == "__main__":
